f813f689305098476eafdfc3945a926f95f2df04
Gabriel de Souza <cladangab@gmail.com>
2016-02-25 14:41:30 -0300

    Update 06
    Basic login system.

--- app/src/main/java/com/example/gabrielsouza/facebook/MainActivity.java
+++ app/src/main/java/com/example/gabrielsouza/facebook/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView labelAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         final Button buttonLogin = (Button) findViewById(R.id.bLogin);
         final EditText fieldLogin = (EditText) findViewById(R.id.loginEnter);
         final EditText fieldPassword = (EditText) findViewById(R.id.passwordEnter);
+        labelAuth = (TextView) findViewById(R.id.relativeLayoutLabelAuth);
 
         buttonLogin.setOnClickListener(new Button.OnClickListener(){
             @Override
@@ -24,8 +29,23 @@ public class MainActivity extends AppCompatActivity {
                 String login = fieldLogin.getText().toString();
                 String pass = fieldPassword.getText().toString();
 
-                //if (login.equals("a") && pass.equals("d"))
+                if (login.equals("milho") && pass.equals("cozido")){
+                    labelAuth.setText(getString(R.string.ok));
+                    Toast.makeText(v.getContext(), "You're logged", Toast.LENGTH_SHORT).show();
+                }else{
+                    labelAuth.setText(getString(R.string.wrong));
+                    Toast.makeText(v.getContext(), "Incorrect Data", Toast.LENGTH_SHORT).show();
+                }
+                labelAuth.setVisibility(View.VISIBLE);
             }
         });
+
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        labelAuth.setVisibility(View.INVISIBLE);
     }
 }
